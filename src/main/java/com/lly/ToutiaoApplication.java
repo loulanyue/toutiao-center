169ead8bbd37377ecfcfe53352e688b7ee462321
GitYue <260355617@qq.com>
2019-01-01 21:04:08 +0800

    Update ToutiaoApplication.java

--- src/main/java/com/lly/ToutiaoApplication.java
+++ src/main/java/com/lly/ToutiaoApplication.java
@@ -2,7 +2,7 @@ package com.lly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class ToutiaoApplication {
 
